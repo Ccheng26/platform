b00a83c1fddec394c1492f54a4a7d1722e3fbce0
Connie Cheng <connie.cheng52@gmail.com>
2017-08-21 23:56:53 -0400

    add initial java file

---
+++ conniecheng-java-homework/src/com/cognizant/introduction_to_java_activity/MyProgram.java
@@ -0,0 +1,7 @@
+class MyProgram extends DisplayMessage{
+  public static void main(String args[]){
+  //System.out.println("My First Java Program");
+  MyProgram program= new MyProgram();
+  program.printMessage();
+ }
+}
